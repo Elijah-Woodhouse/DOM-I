8f292509ebfb86f3351e074ec25afdd6c1d31da9
Elijah Woodhouse <elijahwoodhouse95@gmail.com>
2022-07-11 12:40:08 -0600

    logo, nav links and h1 all loaded correctly using the JSON object in the javascript file.

--- src/index.js
+++ src/index.js
@@ -39,4 +39,24 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
   },
 };
 
-console.log('project wired!')
+console.log('project wired!');
+
+//This sets all the nav links words using a for loop. navigation is turned
+//into an array and then updated using bracket[notation] to what is equal
+//that key value pair in the JSON object of selectros and updated values.
+// the specific selectors used are "nav", "nav-item-'i'", "italic".
+const navigation = document.querySelectorAll('a');
+const arr1 = Array.from(navigation);
+for (let i = 0; i < navigation.length; i++) {
+    arr1[i].textContent = siteContent["nav"][`nav-item-${i}`];
+    arr1[i].classList.add("italic");
+  }
+
+// uses siteContent to set the logo for the DOM by querySelectoring the logo-img id selector.
+const topImage = document.querySelector("#logo-img");
+topImage.src = siteContent["images"]["logo-img"];
+
+
+// uses siteContent to update the H1 value in the div with the class .cta-text.
+const div1 = document.querySelector(".cta-text h1");
+div1.textContent = siteContent["cta"]["h1"];
