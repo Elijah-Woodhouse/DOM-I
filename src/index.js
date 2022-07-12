f70fb1234a2cd11de54a202f449c5eb5bb7a2e11
Elijah Woodhouse <elijahwoodhouse95@gmail.com>
2022-07-11 18:45:16 -0600

    completed project. All tests passed

--- src/index.js
+++ src/index.js
@@ -45,18 +45,85 @@ console.log('project wired!');
 //into an array and then updated using bracket[notation] to what is equal
 //that key value pair in the JSON object of selectros and updated values.
 // the specific selectors used are "nav", "nav-item-'i'", "italic".
-const navigation = document.querySelectorAll('a');
+const navigation = document.querySelectorAll('nav a');
 const arr1 = Array.from(navigation);
 for (let i = 0; i < navigation.length; i++) {
-    arr1[i].textContent = siteContent["nav"][`nav-item-${i}`];
+    arr1[i].textContent = siteContent["nav"][`nav-item-${i + 1}`];
     arr1[i].classList.add("italic");
   }
 
 // uses siteContent to set the logo for the DOM by querySelectoring the logo-img id selector.
+//Images
 const topImage = document.querySelector("#logo-img");
 topImage.src = siteContent["images"]["logo-img"];
-
+const ctaImage = document.querySelector("#cta-img");
+ctaImage.src = siteContent["images"]["cta-img"];
+const middleImage = document.querySelector("#middle-img");
+middleImage.src = siteContent["images"]["accent-img"];
 
 // uses siteContent to update the H1 value in the div with the class .cta-text.
 const div1 = document.querySelector(".cta-text h1");
 div1.textContent = siteContent["cta"]["h1"];
+
+//top button in html document in the .cta class.
+const topButton = document.querySelector(".cta-text button");
+topButton.textContent = siteContent["cta"]["button"];
+
+
+// This is all the code for the the H4's and paragraphs .top-content class in html document.
+const topContentH4 = document.querySelectorAll(".top-content .text-content H4");
+const topContentParagraphs = document.querySelectorAll(".top-content .text-content p");
+const topContentArrayH4 = Array.from(topContentH4);
+const topContentarrayP = Array.from(topContentParagraphs);
+topContentH4[0].textContent = siteContent["main-content"]["features-h4"];
+topContentH4[1].textContent = siteContent["main-content"]["about-h4"];
+topContentarrayP[0].textContent = siteContent["main-content"]["features-content"];
+topContentarrayP[1].textContent = siteContent["main-content"]["about-content"];
+
+
+// bottom content H4 and Paragraph content. .bottom-content class in the html document.
+const bottomContentH4 = document.querySelectorAll(".bottom-content .text-content h4");
+const bottomContentParagraphs = document.querySelectorAll(".bottom-content .text-content p");
+const bottomContentArrayH4 = Array.from(bottomContentH4);
+const bottomContentArrayP = Array.from(bottomContentParagraphs);
+
+// function loopBottomContent(arg1, arg2, arg3){
+//   for (let i = 0; i < bottomContentArrayP.length; i++){
+//     bottomContentH4[i].textContent = siteContent["main-content"][`${arg1}-h4`];
+//     bottomContentH4[i].textContent = siteContent["main-content"][`${arg2}-h4`];
+//     bottomContentH4[i].textContent = siteContent["main-content"][`${arg3}-h4`];
+//     bottomContentParagraphs[i].textContent = siteContent["main-content"][`${arg1}-content`];
+//     bottomContentParagraphs[i].textContent = siteContent["main-content"][`${arg2}-content`];
+//     bottomContentParagraphs[i].textContent = siteContent["main-content"][`${arg3}-content`];
+//   }
+// }
+//
+// loopBottomContent("services", "product", "vision");
+
+bottomContentH4[0].textContent = siteContent["main-content"]["services-h4"];
+bottomContentH4[1].textContent = siteContent["main-content"]["product-h4"];
+bottomContentH4[2].textContent = siteContent["main-content"]["vision-h4"];
+bottomContentParagraphs[0].textContent = siteContent["main-content"]["services-content"];
+bottomContentParagraphs[1].textContent = siteContent["main-content"]["product-content"];
+bottomContentParagraphs[2].textContent = siteContent["main-content"]["vision-content"];
+
+// adding Bold class to footer element.
+const footer = document.querySelector("footer a");
+footer.classList.add("bold");
+footer.textContent = siteContent["footer"]["copyright"];
+
+const contactSectionParagraphs1 = document.querySelector(".contact");
+const contactSectionParagraphs = contactSectionParagraphs1.querySelectorAll("p");
+const contactSectionParagraphArray = Array.from(contactSectionParagraphs);
+
+
+contactSectionParagraphArray[0].textContent = siteContent["contact"]["address"];
+contactSectionParagraphArray[1].textContent = siteContent["contact"]["phone"];
+contactSectionParagraphArray[2].textContent = siteContent["contact"]["email"];
+const contactH4 = document.querySelector(".contact h4");
+contactH4.textContent = siteContent["contact"]["contact-h4"];
+
+
+
+
+///end of document
